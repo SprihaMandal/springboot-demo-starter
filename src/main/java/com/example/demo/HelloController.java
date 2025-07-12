4db607c5f7eedecd80636b690b41e08b3620e93e
Spriha Mandal <spriha2103@gmail.com>
2025-07-12 22:04:39 +0530

    validations added

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,17 +1,22 @@
 package com.example.demo;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
 
 @RestController
 public class HelloController {
 
-     @Autowired
+    @Autowired
     private UserRepository userRepository;
 
     // Simple GET endpoint
@@ -34,38 +39,44 @@ public class HelloController {
 
     @GetMapping("/bye")
     public String sayBye(@RequestParam String user) {
-        return "Bye, "+ user;
+        return "Bye, " + user;
     }
 
-    
     // @PostMapping("/register")
     // public String registerUser(@RequestBody User user) {
-    //     return "Registered: " + user.username + ", " + user.email;
+    // return "Registered: " + user.username + ", " + user.email;
     // }
 
     @PostMapping("/register")
-    public String registerUser(@RequestBody User user) {
+    public String registerUser(@RequestBody @Valid User user) {
         userRepository.save(user);
         return "User registered in MongoDB: " + user.username;
     }
-    
 
-    //fetch user from mongo db
+    // fetch user from mongo db
     @GetMapping("/users")
     public List<User> getAllUsers() {
-    return userRepository.findAll();    
+        return userRepository.findAll();
     }
 
-@GetMapping("/users/{id}")
-public ResponseEntity<?> getUserById(@PathVariable String id) {
-    Optional<User> user = userRepository.findById(id);
-    
-    if (user.isPresent()) {
-        return ResponseEntity.ok(user.get());
-    } else {
-        return ResponseEntity.status(404).body("User not found");
-    }
-}
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable String id) {
+        Optional<User> user = userRepository.findById(id);
 
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.status(404).body("User not found");
+        }
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
+    }
 
 }
--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -3,13 +3,21 @@ package com.example.demo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
 @Document(collection = "users")  // MongoDB collection name
 public class User {
 
     @Id
     public String id;
 
+    @NotBlank(message = "Username cannot be blank")
     public String username;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be blank")
     public String email;
 
     // Getters and setters (optional for now)
